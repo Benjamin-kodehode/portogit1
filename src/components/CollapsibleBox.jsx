1d8b8baf2ac36c6ff08f06aa5e53369f37f6e9f7
Benjamin-kodehode <benjamin2.kodehode@gmail.com>
2025-02-06 23:23:41 +0100

    first commit

---
+++ src/components/CollapsibleBox.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CollapsibleBox({ title, isOpen, onClick }) {
+  return <div></div>;
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ToggleButton from "./ToggleButton";
+ {
+  return (
+  );
+}
